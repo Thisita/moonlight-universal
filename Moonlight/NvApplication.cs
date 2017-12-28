7d2b8a6e13c414961ae68f9529f14e1929d902cd
Ian Zachary Ledrick <Thisita@users.noreply.github.com>
2017-12-27 19:08:43 -0500

    Add new data models for applications

---
+++ Moonlight/NvApplication.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+namespace Moonlight
+{
+    public class NvApplication
+    {
+        public int ID { get; set; }
+        [XmlElement(ElementName = "AppTitle")]
+        public string Title { get; set; }
+        public int IsHdrSupported { get; set; }
+        [XmlIgnore]
+        public string BoxArt { get; set; }
+    }
+}
---
+++ Moonlight/NvApplicationList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Moonlight
+{
+    [XmlRoot(ElementName = "root")]
+    public class NvApplicationList
+    {
+        [XmlAttribute("protocol_version")]
+        public string ProtocolVersion { get; set; }
+        [XmlAttribute("query")]
+        public string Query { get; set; }
+        [XmlAttribute("status_code")]
+        public int StatusCode { get; set; }
+        [XmlAttribute("status_message")]
+        public string StatusMessage { get; set; }
+
+        public List<NvApplication> Applications { get; set; }
+    }
+}
