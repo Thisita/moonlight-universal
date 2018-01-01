555a60ff3b1b1c6c413f2518e03ecf2335ebc572
Ian Zachary Ledrick <Thisita@users.noreply.github.com>
2017-12-31 21:49:12 -0500

    Update crypto provider to load client cert into certificate store for use by httpclient

--- Moonlight/CryptoProvider.cs
+++ Moonlight/CryptoProvider.cs
@@ -1,4 +1,6 @@
-﻿using Org.BouncyCastle.Asn1.X509;
+﻿using Org.BouncyCastle.Asn1;
+using Org.BouncyCastle.Asn1.Pkcs;
+using Org.BouncyCastle.Asn1.X509;
 using Org.BouncyCastle.Crypto;
 using Org.BouncyCastle.Crypto.Generators;
 using Org.BouncyCastle.Crypto.Modes;
@@ -6,15 +8,21 @@ using Org.BouncyCastle.Crypto.Operators;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.Math;
 using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Pkcs;
 using Org.BouncyCastle.Security;
 using Org.BouncyCastle.X509;
 using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Security.Cryptography;
+using Windows.Security.Cryptography.Certificates;
+using Windows.Security.Cryptography.Core;
 using Windows.Storage;
+using Windows.Storage.Streams;
 
 namespace Moonlight
 {
@@ -26,7 +34,7 @@ namespace Moonlight
         private const string CERTIFICATE_FILE_NAME = "client.crt";
         private static SecureRandom SecureRandom = new SecureRandom();
         private AsymmetricKeyParameter Key { get; set; }
-        public X509Certificate Certificate { get; private set; }
+        public Org.BouncyCastle.X509.X509Certificate Certificate { get; private set; }
 
         public static byte[] GenerateRandomBytes(int length)
         {
@@ -98,7 +106,7 @@ namespace Moonlight
             return cipher.DoFinal(blockRoundedData);
         }
 
-        public static bool VerifySignature(byte[] data, byte[] signature, X509Certificate certificate)
+        public static bool VerifySignature(byte[] data, byte[] signature, Org.BouncyCastle.X509.X509Certificate certificate)
         {
             ISigner signer = SignerUtilities.GetSigner(SIGNING_ALGO);
             signer.Init(false, certificate.GetPublicKey());
@@ -145,6 +153,58 @@ namespace Moonlight
             Certificate = certificateGenerator.Generate(signatureFactory);
         }
 
+        private async Task<bool> IsKeyPairInStore()
+        {
+            CertificateQuery query = new CertificateQuery()
+            {
+                FriendlyName = Certificate.SubjectDN.ToString()
+            };
+            return (await CertificateStores.FindAllAsync(query)).Count > 0;
+        }
+
+        public async Task<Certificate> GetClientSslCertificate()
+        {
+            CertificateQuery query = new CertificateQuery()
+            {
+                FriendlyName = Certificate.SubjectDN.ToString()
+            };
+            Certificate cert = (await CertificateStores.FindAllAsync(query)).FirstOrDefault();
+            bool verify = await VerifyCertificateKeyAccess(cert);
+            return cert;
+        }
+
+        private async Task<bool> VerifyCertificateKeyAccess(Certificate certificate)
+        {
+            CryptographicKey cryptographicKey = await PersistedKeyProvider.OpenKeyPairFromCertificateAsync(certificate, HashAlgorithmNames.Sha1, CryptographicPadding.RsaPkcs1V15);
+            String buffer = "Test data";
+            IBuffer data = CryptographicBuffer.ConvertStringToBinary(buffer, BinaryStringEncoding.Utf16BE);
+            try
+            {
+                IBuffer signed = await CryptographicEngine.SignAsync(cryptographicKey, data);
+                return CryptographicEngine.VerifySignature(cryptographicKey, data, signed);
+            }
+            catch (System.Exception exp)
+            {
+                System.Diagnostics.Debug.WriteLine("Verification Failed. Excepion Occurred : {0}", exp.Message);
+                return false;
+            }
+        }
+
+        private async Task LoadKeyPairToStore()
+        {
+            var p12Store = new Pkcs12Store();
+            var certEntry = new X509CertificateEntry(Certificate);
+            p12Store.SetCertificateEntry(Certificate.SubjectDN.ToString(), certEntry);
+            p12Store.SetKeyEntry(Certificate.SubjectDN.ToString(), new AsymmetricKeyEntry(Key), new[] { certEntry });
+            using (MemoryStream pfxStream = new MemoryStream())
+            {
+                p12Store.Save(pfxStream, string.Empty.ToCharArray(), SecureRandom);
+                pfxStream.Seek(0, SeekOrigin.Begin);
+
+                await CertificateEnrollmentManager.ImportPfxDataAsync(Convert.ToBase64String(pfxStream.ToArray()), string.Empty, ExportOption.Exportable, KeyProtectionLevel.NoConsent, InstallOptions.DeleteExpired, Certificate.SubjectDN.ToString());
+            }
+        }
+
         public string GetCertificatePem()
         {
             using (TextWriter textWriter = new StringWriter())
@@ -186,7 +246,7 @@ namespace Moonlight
                 using (TextReader textReader = new StringReader(certificatePem))
                 {
                     PemReader pemReader = new PemReader(textReader);
-                    Certificate = pemReader.ReadObject() as X509Certificate;
+                    Certificate = pemReader.ReadObject() as Org.BouncyCastle.X509.X509Certificate;
                 }
                 StorageFile keyFile = await localFolder.GetFileAsync("client.key");
                 string keyPem = await FileIO.ReadTextAsync(keyFile);
@@ -195,6 +255,10 @@ namespace Moonlight
                     PemReader pemReader = new PemReader(textReader);
                     Key = (pemReader.ReadObject() as AsymmetricCipherKeyPair).Private;
                 }
+                //if (!(await IsKeyPairInStore()))
+                //{
+                    await LoadKeyPairToStore();
+                //}
             }
             catch(System.Exception)
             {
@@ -203,14 +267,23 @@ namespace Moonlight
             }
         }
 
-        public static X509Certificate HexStringToX509Certificate(string hex)
+        public static Org.BouncyCastle.X509.X509Certificate HexStringToX509Certificate(string hex)
         {
             byte[] data = StringToByteArray(hex);
             string certificatePem = Encoding.UTF8.GetString(data);
             using (TextReader textReader = new StringReader(certificatePem))
             {
                 PemReader pemReader = new PemReader(textReader);
-                return pemReader.ReadObject() as X509Certificate;
+                return pemReader.ReadObject() as Org.BouncyCastle.X509.X509Certificate;
+            }
+        }
+
+        private static void AddCertificateToStore(X509Certificate2 certificate, StoreName storeName, StoreLocation storeLocation)
+        {
+            using (X509Store store = new X509Store(storeName, storeLocation))
+            {
+                store.Open(OpenFlags.ReadWrite);
+                store.Add(certificate);
             }
         }
 
