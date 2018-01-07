e578ec2b0aea5bfc43a7e121607f5b86255b9f3c
Ian Zachary Ledrick <Thisita@users.noreply.github.com>
2018-01-06 20:59:02 -0500

    Create a configuration class to make things look a lot cleaner

---
+++ Moonlight/Configuration.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Storage;
+
+namespace Moonlight
+{
+    public class Configuration
+    {
+        private static readonly ApplicationDataContainer _localSettings = ApplicationData.Current.LocalSettings;
+
+        public static Guid UniqueUuid
+        {
+            get
+            {
+                if (_localSettings.Values.ContainsKey("NvHttp.UniqueUuid"))
+                {
+                    return (Guid)_localSettings.Values["NvHttp.UniqueUuid"];
+                }
+                else
+                {
+                    Guid newValue = Guid.NewGuid();
+                    _localSettings.Values["NvHttp.UniqueUuid"] = newValue;
+                    return newValue;
+                }
+            }
+        }
+
+        public static bool LocalAudio
+        {
+            get
+            {
+                if (_localSettings.Values.ContainsKey("LocalAudio"))
+                {
+                    return (bool)_localSettings.Values["LocalAudio"];
+                }
+                else
+                {
+                    _localSettings.Values["LocalAudio"] = false;
+                    return false;
+                }
+            }
+            set
+            {
+                _localSettings.Values["LocalAudio"] = value;
+            }
+        }
+
+        public static string DeviceName
+        {
+            get
+            {
+                if (_localSettings.Values.ContainsKey("DeviceName"))
+                {
+                    return (string)_localSettings.Values["DeviceName"];
+                }
+                else
+                {
+                    string newValue = Dns.GetHostName();
+                    _localSettings.Values["DeviceName"] = newValue;
+                    return newValue;
+                }
+            }
+        }
+    }
+}
